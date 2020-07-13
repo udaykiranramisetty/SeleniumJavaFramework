f0c1f67313790341930eb09e2882a4a745ce5140
udaykir101 <udaykir101@IN5CD8070P3Y.IN.nam.ad.pwcinternal.com>
2020-07-13 10:47:32 +0530

    Second Commit

---
+++ SeleniumBasic/src/test/java/com/Selenium/TestGitHub.java
@@ -0,0 +1,14 @@
+package com.Selenium;
+
+public class TestGitHub {
+	
+	static String Name;
+	static String Bank;
+	
+	public static void main(String[] args) {
+		Name = "Uday";
+		Bank = "Kotak";
+			System.out.println("My Name is  "+Name+" and I have an account in "+Bank);
+	}
+
+}
